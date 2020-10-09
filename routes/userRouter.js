eff16bbd5f1a746befbf2d9b5e04733ec8c4acb6
GAURAV <gauravpatil8433@gmail.com>
2020-10-09 20:59:16 +0530

    auth complete

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -100,4 +100,11 @@ router.post("/tokenIsValid", async (req,res)=>{
     }
 });
 
+router.get("/",auth,async (req,res)=>{
+    const user = await User.findById(req.user);
+    res.json({
+        username:user.username,
+        id:user._id
+    });
+});
 module.exports = router;
